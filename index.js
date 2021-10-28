85f0d0a1f72658e4da780bdcb9a5b04f347c94dd
SHAIFULislamSHAKIB0 <cseshaifulislam@gmail.com>
2021-10-28 12:26:50 +0600

    cmnt added

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ const ObjectId = require('mongodb').ObjectId;
 
 const app = express();
 const port = process.env.PORT || 5000;
-// middleware
+
+// middleware we use here
 app.use(cors());
 app.use(express.json());
 
